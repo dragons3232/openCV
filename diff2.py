9e4ec1ce8989f43c70c517447c70be0f83e6bf66
long.phan <dragons3232@gmail.com>
2024-05-26 17:05:45 +0700

    CV findContours

--- diff2.py
+++ diff2.py
@@ -3,7 +3,11 @@ from matplotlib import image as mpimg
 
 
 im1 = cv2.imread("p1.jpg")
+cv2.imshow("image BGR", im1)
+cv2.waitKey(0)
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
+cv2.imshow("image RGB", im1)
+cv2.waitKey(0)
 im2 = mpimg.imread("p2.jpg")
 
 # XOR with CV2
@@ -11,3 +15,24 @@ result = cv2.bitwise_xor(im1, im2)
 
 cv2.imshow("result image", result)
 cv2.waitKey(0)
+
+# Grayscale
+gray = cv2.cvtColor(result, cv2.COLOR_RGB2GRAY)
+cv2.imshow("gray image", gray)
+cv2.waitKey(0)
+
+# Find Canny edges
+edged = cv2.Canny(gray, 30, 200)
+cv2.imshow("edged image", edged)
+cv2.waitKey(0)
+
+# Finding Contours
+contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+# Draw all contours
+cv2.drawContours(im1, contours, -1, (0, 255, 0), 3)
+
+im1 = cv2.cvtColor(im1, cv2.COLOR_RGB2BGR)
+cv2.imshow("Contours", im1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
