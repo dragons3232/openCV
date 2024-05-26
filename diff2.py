f08128e5457127321c2ee9d26da0b5479e0d3066
long.phan <dragons3232@gmail.com>
2024-05-26 22:46:37 +0700

    Draw connection lines between images

--- diff2.py
+++ diff2.py
@@ -1,14 +1,10 @@
 import cv2
-from matplotlib import image as mpimg
+from matplotlib import pyplot as plt, image as mpimg
+from matplotlib.patches import ConnectionPatch
 
 
 im1 = cv2.imread("p1.jpg")
-cv2.imshow("image BGR", im1)
-cv2.waitKey(0)
-im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
-cv2.imshow("image RGB", im1)
-cv2.waitKey(0)
-im2 = mpimg.imread("p2.jpg")
+im2 = cv2.imread("p2.jpg")
 
 # XOR with CV2
 result = cv2.bitwise_xor(im1, im2)
@@ -35,9 +31,40 @@ for contour in contours:
     if w > 150 and h > 150:
         filteredContours.append(contour)
         cv2.rectangle(im1, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
 # # Draw all contours
 # cv2.drawContours(im1, filteredContours, -1, (0, 255, 0), 3)
+
+# Draw lines between points on the two images
+fig, axes = plt.subplots(1, 2)
+ax1 = axes[0]
+ax2 = axes[1]
+
+ax1.imshow(im1)
+ax1.set_title("Image 1")
+ax1.axis("off")
+ax2.imshow(im2)
+ax2.set_title("Image 2")
+ax2.axis("off")
+
+for cnt in filteredContours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    points = [(x, y), (x + w, y + h)]
+    for point in points:
+        xy = point
+        con = ConnectionPatch(
+            xyA=xy,
+            xyB=xy,
+            coordsA="data",
+            coordsB="data",
+            axesA=ax2,
+            axesB=ax1,
+            color="green",
+        )
+        ax2.add_artist(con)
+
+plt.show()
 
 im1 = cv2.cvtColor(im1, cv2.COLOR_RGB2BGR)
 cv2.imshow("Contours", im1)
